37363aa26954e9de11bfc149e6348437b80e6b7e
Travis McChesney <travis@travismcchesney.com>
2012-07-02 17:42:59 -0600

    Completed required task 3 - sin cos sqrt pi

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -53,6 +53,14 @@
     } else if ([operation isEqualToString:@"/"]) {
         double divisor = [self popOperand];
         if (divisor) result = [self popOperand] / divisor;
+    } else if ([operation isEqualToString:@"sin"]) {
+        result = sin([self popOperand]);
+    } else if ([operation isEqualToString:@"cos"]) {
+        result = cos([self popOperand]);
+    } else if ([operation isEqualToString:@"√"]) {
+        result = sqrt([self popOperand]);
+    } else if ([operation isEqualToString:@"π"]) {
+        result = M_PI;
     }
     
     [self pushOperand:result];
